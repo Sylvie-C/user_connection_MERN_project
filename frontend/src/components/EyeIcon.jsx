ca4566378d6d41ceaf91527031a6915e12529b21
CSylvie <sylvie.castet@gmail.com>
2024-07-16 00:39:11 +0200

    signin form updates

---
+++ frontend/src/components/EyeIcon.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react" ; 
+
+
+export default function EyeIcon ( { eyeClicked } ) {
+
+  const [ eyeOpened , setOpen ] = useState (true) ; 
+
+  let eyeIcon
+
+  if (eyeOpened) {
+    eyeIcon = String.fromCodePoint(128065) ; 
+  }else{
+    eyeIcon = '\u25E1' ; 
+  }
+
+  const openEye = () => { 
+    setOpen (!eyeOpened) ; 
+    eyeClicked(eyeOpened); 
+  }
+
+  return (
+    <div>
+      <span onClick={ openEye } className="mx-2 hover:cursor-pointer">
+        { eyeIcon } 
+      </span>
+    </div>
+  )
+}
--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -1,8 +1,43 @@
+import { useState } from "react" ; 
 import { useNavigate } from "react-router-dom" ; 
+
+import EyeIcon from "./EyeIcon";
 
 export default function Signin () {
 
+  const [ pwd01Visibility , setPwd01Visibility ] = useState (false) ;  
+  const [ pwd02Visibility , setPwd02Visibility ] = useState (false) ; 
   const navigate = useNavigate() ; 
+
+  // display password on clic on eye icon (password hidden by default)
+  const showPassword01 = (value) => {
+    // if eye is closed, password is visible
+    if (value) {
+      setPwd01Visibility (true) ; 
+    }else{
+      setPwd01Visibility (false) ; 
+    }
+  }
+
+  const showPassword02 = (value) => {
+    let show ; 
+
+    // if eye is closed, password is visible
+    if (value) {
+      setPwd02Visibility (true) ; 
+    }else{
+      setPwd02Visibility (false) ; 
+    }
+  }
+
+  // check if password confirmed (2 passwords identical)
+  const checkPwd = (pwd01 , pwd02) => {
+    pwd01 = pwd01.trim() ; 
+    pwd02 = pwd02.trim() ; 
+
+    if (pwd01 === pwd02) { return true }
+    else{ return false }
+  }
 
   // on form submit
   const handleSubmit = async (event) => {
@@ -10,64 +45,98 @@ export default function Signin () {
     event.preventDefault() ; 
 
     const formDataObj = new FormData (event.target) ; 
-    console.log (formDataObj) ; 
 
-    const data = {
-      name: formDataObj.get("name") ,
-      surname: formDataObj.get("surname") , 
-      username: formDataObj.get("username") , 
-      email: formDataObj.get("email") , 
-    } 
+    const pwdConfirmed = checkPwd (formDataObj.get("pwd01") , formDataObj.get("pwd02")) ; 
 
-    try {
-      const response = await fetch (
-        `${import.meta.env.VITE_BACKEND_URL}/api/user/` , // route defined in backend /user.js file
-        {
-          method: "POST" , 
-          headers: { "Content-Type" : "application/json" } , 
-          body: JSON.stringify(data) , 
+    if (pwdConfirmed) {
+      const data = {
+        username: formDataObj.get("username") , 
+        email: formDataObj.get("email") , 
+        password: pwdConfirmed
+      } 
+  
+      try {
+        const response = await fetch (
+          `${import.meta.env.VITE_BACKEND_URL}/api/user/` , // route defined in backend /user.js file
+          {
+            method: "POST" , 
+            headers: { "Content-Type" : "application/json" } , 
+            body: JSON.stringify(data) , 
+          }
+        )
+  
+        if (response.ok) {
+          alert("Welcome ! You registered successfully.") ; 
+          navigate("/login") ; 
+        }else{
+          alert("Oops ! There was a problem. You are not registered. ") ; 
         }
-      )
-
-      if (response.ok) {
-        alert("Welcome ! You registered successfully.") ; 
-        navigate("/login") ; 
-      }else{
-        alert("Oops ! There was a problem. You are not registered. ") ; 
       }
-    }
-
-    catch (err) {
-      console.error (err) ; 
+  
+      catch (err) {
+        console.error (err) ; 
+      }
+    }else{
+      alert("Passwords are not identical.") ; 
     }
   }
 
   return (
-    <div className="flex flex-col items-center p-2">
-      <form onSubmit={handleSubmit} className="w-full lg:w-3/5 pt-4 md:pt-10 pb-10 flex flex-col items-center bg-purple-400 rounded-md">
+    <div className="p-2">
+      <form onSubmit={handleSubmit} className="mx-auto w-full sm:w-3/5 pt-4 md:pt-10 pb-10 flex flex-col bg-purple-400 rounded-md">
+        <div className="mx-auto w-72 sm:w-fit sm:px-4">
+          <div className="flex flex-col mb-4">
+            <label htmlFor="username">Your username:</label>
+            <input
+              type="text"
+              name="username"
+              className="w-72 md:w-96 h-10 bg-white px-2 my-2 rounded-md"
+            />
+          </div>
 
-        <h1 className="font-heading text-4xl text-center mb-6">Sign in Form</h1>
-        <label htmlFor="name" >Your first name : </label>
-        <input type="text" name="name" 
-          className="bg-white w-5/6 sm:w-3/6 p-2 rounded-md mb-4"
-        />
+          <div className="flex flex-col mb-4">
+            <label htmlFor="email">Your email:</label>
+            <input
+              required
+              type="email"
+              name="email"
+              className="w-72 md:w-96 h-10 bg-white px-2 my-2 rounded-md"
+            />
+          </div>
 
-        <label htmlFor="surname" >Your family name : </label>
-        <input type="text" name="surname" 
-          className="bg-white w-5/6 sm:w-3/6 p-2 rounded-md mb-4" 
-        />
+          <div className="flex flex-col mb-4">
+            <label htmlFor="password">Your password:</label>
+            <div className="flex items-center">
+              <input
+                required
+                type= { pwd01Visibility ? "text" : "password" }
+                name="pwd01"
+                className="w-72 md:w-96 h-10 bg-white px-2 my-2 rounded-md"
+              />
+              <EyeIcon eyeClicked={ showPassword01 }/>
+            </div>
+          </div>
 
-        <label htmlFor="username" >Your username : </label>
-        <input type="text" name="username" 
-          className="bg-white w-5/6 sm:w-3/6 p-2 rounded-md mb-4" 
-        />
+          <div className="flex flex-col mb-4">
+            <label htmlFor="pwd-confirm">Confirm your password:</label>
+            <div className="flex items-center">
+              <input
+                required
+                type= { pwd02Visibility ? "text" : "password" }
+                name="pwd02"
+                className="w-72 md:w-96 h-10 bg-white px-2 my-2 rounded-md"
+              />
+              <EyeIcon eyeClicked={ showPassword02 }/>
+            </div>
+          </div>
 
-        <label htmlFor="email" >Your email address : </label>
-        <input type="text" name="email" required 
-          className="bg-white w-5/6 sm:w-3/6 p-2 rounded-md mb-4" 
-        />
-
-        <button type="submit" className="w-3/12 lg:w-1/12 text-white bg-purple-950 p-2 m-4 inline-block rounded-xl transition hover:bg-white hover:border-purple-950 border-2 hover:text-purple-950 duration-300">OK</button>
+          <div className="w-72 md:w-96 flex justify-center">
+          <button type="submit" 
+            className="w-40 text-white bg-purple-950 p-2 m-4 inline-block rounded-xl transition hover:bg-white hover:border-purple-950 border-2 hover:text-purple-950 duration-300">
+              OK
+          </button>
+        </div>
+        </div>
       </form>
     </div>
   )
