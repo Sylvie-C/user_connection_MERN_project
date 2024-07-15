29677166bc280a5e15c742aeb9c00f5f897cad45
CSylvie <sylvie.castet@gmail.com>
2024-07-16 00:39:39 +0200

    signin form updates

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,16 +3,13 @@ import { NavLink } from "react-router-dom" ;
 export default function Home () {
 
   return(
-    <div className="flex justify-center">
-      <div className="w-full lg:w-3/5 bg-purple-400 rounded-md flex justify-center ">
+    <div className="w-full lg:max-w-7xl mx-auto bg-purple-400 rounded-md flex justify-center ">
+      <div className="w-full lg:w-2/4 mx-2 ls:mx-16 my-8 lg:my-16 bg-purple-200 rounded-md text-center">
+        <p>Already have an account ? </p>
+        <NavLink to="login" className="mb-8 text-white bg-purple-950 p-2 m-4 inline-block rounded-xl transition hover:bg-white hover:border-purple-950 border-2 hover:text-purple-950 duration-300">Log in</NavLink>
 
-        <div className="w-full lg:w-2/4 my-8 lg:my-16 mx-2 ls:mx-16 bg-purple-200 rounded-md text-center">
-          <p>Already have an account ? </p>
-          <NavLink to="login" className="mb-8 text-white bg-purple-950 p-2 m-4 inline-block rounded-xl transition hover:bg-white hover:border-purple-950 border-2 hover:text-purple-950 duration-300">Log in</NavLink>
-
-          <p>New user ? Create an account</p>
-          <NavLink to="signin" className="text-white bg-purple-950 p-2 m-4 inline-block rounded-xl transition hover:bg-white hover:border-purple-950 border-2 hover:text-purple-950 duration-300">Sign in</NavLink>
-        </div>
+        <p>New user ? Create an account</p>
+        <NavLink to="signin" className="text-white bg-purple-950 p-2 m-4 inline-block rounded-xl transition hover:bg-white hover:border-purple-950 border-2 hover:text-purple-950 duration-300">Sign in</NavLink>
       </div>
     </div>
   )
