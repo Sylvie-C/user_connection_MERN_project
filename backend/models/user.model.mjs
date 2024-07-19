7242d028d36ab27123613d8cc916707eeafc78da
CSylvie <sylvie.castet@gmail.com>
2024-07-19 13:22:40 +0200

    deactivate mongoose versionKey field for User objects in database

--- backend/models/user.model.mjs
+++ backend/models/user.model.mjs
@@ -6,7 +6,8 @@ const userSchema = new mongoose.Schema (
     email: { type: String , required: true , unique: true } , 
     password: { type: String , required: true } , 
     date: { type: Date , default: Date.now },
-  }
+  }, 
+  { versionKey: false }
 ); 
 
 const UserModel = mongoose.model ( 'User' , userSchema ) ; 
