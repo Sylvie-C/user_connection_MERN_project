ba83a19b27d763aab8ad1da3129a55c48c81dbfc
CSylvie <sylvie.castet@gmail.com>
2024-07-19 14:53:51 +0200

    hide port number

--- backend/server.mjs
+++ backend/server.mjs
@@ -12,7 +12,7 @@ import users from "./routes/user.mjs";
 const swaggerDocs = yaml.load ("./swagger.yaml") ; 
 dotenv.config(); 
 const app = express(); 
-const PORT = process.env.PORT || 5050; 
+const PORT = process.env.PORT || ""; 
 
 app.use (cors()) ; 
 app.use (express.json()) ; 
