f7091a62f5bbecf26845804662886622228b949a
CSylvie <sylvie.castet@gmail.com>
2024-07-22 23:28:02 +0200

    add dotenv package to vite.config.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv"
+
+dotenv.config() ; 
 
 // Définir la base URL en fonction de l'environnement
 const base = process.env.NODE_ENV === 'production' ? '/frontend/' : '/'
@@ -9,4 +12,13 @@ const base = process.env.NODE_ENV === 'production' ? '/frontend/' : '/'
 export default defineConfig({
   plugins: [react()],
   base: base, 
+/*   server: {
+    proxy: {
+      '/api': `${process.env.VITE_BACKEND_URL}`
+    }
+  } */
 })
+
+
+
+
