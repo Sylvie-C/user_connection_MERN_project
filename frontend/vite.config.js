acf28834e8a708a2f1658ce43f78259e4750f4ed
CSylvie <sylvie.castet@gmail.com>
2024-07-22 23:47:08 +0200

    delete unecessary code lines

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Définir la base URL en fonction de l'environnement
-// const base = `${import.meta.env.NODE_ENV}` === 'production' ? '/frontend/' : '/'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  /*
-  base: base, 
-  server: {
-    proxy: {
-      '/api': `${process.env.VITE_BACKEND_URL}`
-    }
-  } */
 })
 
 
