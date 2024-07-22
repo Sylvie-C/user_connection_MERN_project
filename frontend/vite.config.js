0976c53e6b7d0fd60388fa662beb62ac1ea7a1cb
CSylvie <sylvie.castet@gmail.com>
2024-07-22 23:41:04 +0200

    test railway no base declared for vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from "dotenv"
-
-dotenv.config() ; 
 
 // Définir la base URL en fonction de l'environnement
-const base = process.env.NODE_ENV === 'production' ? '/frontend/' : '/'
-
+// const base = `${import.meta.env.NODE_ENV}` === 'production' ? '/frontend/' : '/'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  /*
   base: base, 
-/*   server: {
+  server: {
     proxy: {
       '/api': `${process.env.VITE_BACKEND_URL}`
     }
